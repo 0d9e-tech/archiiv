6741cd2c1384b851365d1edfb07ac9dcc501c3fd
Prokop Randacek <prokop@rdck.dev>
2023-09-07 13:07:42 +0200

    update user registration script

--- scripts/register_user.zig
+++ scripts/register_user.zig
@@ -5,14 +5,25 @@ const std = @import("std");
 const cryptoh = @import("src/crypto_helper.zig");
 const fsh = @import("src/fs_helper.zig");
 const User = @import("src/user.zig").User;
+const otp = @import("src/otp.zig");
+const base32 = @import("src/base32.zig");
 
 pub fn main() !void {
-    const users = [_]User{
-        User{
-            .id = 19,
-            .name = "prokop",
-            .otp_secret = undefined,
-        },
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var alc = gpa.allocator();
+
+    const user = User{
+        .id = 19,
+        .name = "prokop",
+        .otp_secret = otp.generateOtpSecret(),
     };
-    try fsh.writeUsers(&users, std.fs.cwd());
+    // We store the secret in memory as raw bytes, save it to disk as base64
+    // and (as the TOTP spec dictates) send to user as base32
+    const b32secret = try base32.encode(alc, &user.otp_secret.value, false);
+    defer alc.free(b32secret);
+
+    const writer = std.io.getStdOut().writer();
+    try writer.print("otpauth://totp/archiv:{s}?secret={s}&issuer=archiv\n", .{ user.name, b32secret });
+    try fsh.writeUsers(&[_]User{user}, std.fs.cwd());
 }
