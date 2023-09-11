65d2325e1888d1604c6d14168ec644ca712865bb
Prokop Randacek <prokop@rdck.dev>
2023-09-11 11:54:36 +0200

    handle ls open dir errors

--- src/ls_endpoint.zig
+++ src/ls_endpoint.zig
@@ -4,10 +4,13 @@ const fs = std.fs;
 const http = std.http;
 
 pub fn handle(user_dir: fs.Dir, path: []const u8, sink: anytype) !http.Status {
-    var target_dir = try user_dir.openIterableDir(
+    var target_dir = user_dir.openIterableDir(
         if (path.len == 0) "./" else path,
         .{},
-    );
+    ) catch |e| return switch (e) {
+        error.NotDir, error.FileNotFound, error.BadPathName, error.InvalidUtf8 => .not_found,
+        else => e,
+    };
     defer target_dir.close();
 
     try constructJson(&target_dir, sink);
@@ -24,4 +27,3 @@ fn constructJson(root: *fs.IterableDir, sink: anytype) !void {
     }
     try jws.endArray();
 }
-
