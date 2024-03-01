d4ce0d5656013f587361104b59913cbebd0e99cf
Prokop Randacek <prokop@rdck.dev>
2024-03-01 11:00:51 +0100

    simplify main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -9,16 +8,13 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"sync"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 func main() {
-	ctx := context.Background()
-	if err := run(ctx, os.Stdout, os.Args, os.Getenv); err != nil {
+	if err := run(os.Stdout, os.Args, os.Getenv); err != nil {
 		fmt.Printf("error from main: %s\n", err)
 		os.Exit(1)
 	}
@@ -43,29 +39,59 @@ func newServer(
 	return handler
 }
 
-func getConfig(args []string, env func(string) string) (host string, port string, secret string, fs_root string, err error) {
+type config struct {
+	host      string
+	port      string
+	secret    string
+	fs_root   string
+	root_uuid uuid.UUID
+}
+
+func getConfig(args []string, env func(string) string) (conf config, err error) {
 	flags := flag.NewFlagSet("archiiv", flag.ContinueOnError)
 
-	flags.StringVar(&host, "host", "localhost", "")
-	flags.StringVar(&port, "port", "8275", "")
+	flags.StringVar(&conf.host, "host", "localhost", "")
+	flags.StringVar(&conf.port, "port", "8275", "")
+	flags.StringVar(&conf.fs_root, "fs_root", "", "")
 
-	secret = env("ARCHIIV_SECRET")
-	fs_root = env("ARCHIIV_FS")
+	var root_uuid_string string
+
+	flags.StringVar(&root_uuid_string, "root_uuid", "", "")
+
+	conf.secret = env("ARCHIIV_SECRET")
 
 	err = flags.Parse(args[1:])
+	if err != nil {
+		return
+	}
+
+	conf.root_uuid, err = uuid.Parse(root_uuid_string)
 
 	return
 }
 
-func run(ctx context.Context, w io.Writer, args []string, env func(string) string) error {
-	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
-	defer cancel()
+func greet(log *slog.Logger) {
+	hour := time.Now().Hour()
+	switch {
+	case hour < 12:
+		log.Info("Good morning")
+	case hour < 17:
+		log.Info("Good afternoon")
+	default:
+		log.Info("Good evening")
+	}
+}
 
+func goodbye(log *slog.Logger) {
+	log.Info("Goodbye")
+}
+
+func run(w io.Writer, args []string, env func(string) string) error {
 	log := slog.New(slog.NewJSONHandler(w, nil))
-	log.Info("Dobrý den")
-	defer log.Info("Nashledanou")
+	greet(log)
+	defer goodbye(log)
 
-	host, port, secret, fs_root, err := getConfig(args, env)
+	conf, err := getConfig(args, env)
 	if err != nil {
 		return err
 	}
@@ -75,39 +101,23 @@ func run(ctx context.Context, w io.Writer, args []string, env func(string) strin
 		return err
 	}
 
-	files, err := newFs(uuid.Nil, fs_root)
+	files, err := newFs(conf.root_uuid, conf.fs_root)
 	if err != nil {
 		return err
 	}
 
-	srv := newServer(log, secret, users, files)
+	srv := newServer(log, conf.secret, users, files)
 
 	httpServer := &http.Server{
-		Addr:    net.JoinHostPort(host, port),
+		Addr:    net.JoinHostPort(conf.host, conf.port),
 		Handler: srv,
 	}
 
-	go func() { // listening goroutine
-		log.Info("listening", "address", httpServer.Addr)
-		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Error("listening and serving", "error", err)
-		}
-	}()
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go func() { // cleanup goroutine
-		defer wg.Done()
-		<-ctx.Done()
-
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		if err := httpServer.Shutdown(shutdownCtx); err != nil {
-			log.Error("shutting down http server", "error", err)
-		}
-	}()
-	wg.Wait()
+	log.Info("listening", "address", httpServer.Addr)
+	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Error("listening and serving", "error", err)
+		return err
+	}
 
 	return nil
 }
